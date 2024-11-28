5ac07b77ae40eaf283f12cd43a13c025bee3916e
Rishabh Jain <rishabh2824@gmail.com>
2024-11-27 23:58:26 -0600

    fixing cors

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app = FastAPI()
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://teekoai.netlify.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
